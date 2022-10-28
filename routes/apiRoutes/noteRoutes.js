e6dbb832787652b9d4ebc3b6bc8bccdc35515bc5
mjd10m <mjd10m@outlook.com>
2022-10-27 22:53:42 -0400

    delete route added

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,7 +1,6 @@
-const { createNewNote } = require('../../lib/note')
+const { createNewNote, deleteNote } = require('../../lib/note')
 const { notes } = require('../../database/db')
 const router = require('express').Router();
-console.log(notes)
 
 router.get('/notes', (req, res) => {
     const result = notes;
@@ -16,5 +15,14 @@ router.post('/notes', (req, res) => {
         const note = createNewNote(req.body, notes);
         res.json(note);
 });
+
+router.delete('/notes/:id', (req, res) => {
+    const result = deleteNote(req.params.id, notes);
+    if(result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }    
+})
 
 module.exports = router
